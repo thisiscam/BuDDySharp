47c2274152661f273cda1601760fae1bec6e4cc8
jq.yang@berkeley.edu <jq.yang@berkeley.edu>
2016-06-21 19:20:30 -0700

    add test project

---
+++ test/testBuDDySharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using buddysharp;
+
+namespace testBuDDySharp
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			bdd.bdd_init (100, 1000);
+			var t = bdd.bdd_true ();
+			Console.WriteLine (t.ToString ());
+		}
+	}
+}
