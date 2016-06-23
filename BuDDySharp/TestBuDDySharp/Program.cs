704e3bf18e952b5c5043f98a6163b4c01a6d9133
jq.yang@berkeley.edu <jq.yang@berkeley.edu>
2016-06-23 16:11:05 -0700

    cpp interface one

---
+++ BuDDySharp/TestBuDDySharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BuDDySharp;
+using System.Runtime.InteropServices;
+
+namespace BuDDySharp.Test
+{
+	class MainClass
+	{
+		public unsafe static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			BuDDySharp.cpp_init (1000, 1000);
+			BuDDySharp.setvarnum (100);
+			var t = BuDDySharp.bvec_true(12);
+			var f = BuDDySharp.bvec_map1 (t, test);
+			BuDDySharp.bvec_map2 (f, t, (a, b) => {
+				var tmp = a.Or(b);
+				Console.WriteLine(tmp.EqualEqual(BuDDySharp.bddtrue));	
+				return tmp;
+			});
+		}
+		public static bdd test(bdd x) {
+			BuDDySharp.printdot (x);
+			return x.And (BuDDySharp.bddfalse);
+		}
+	}
+}
